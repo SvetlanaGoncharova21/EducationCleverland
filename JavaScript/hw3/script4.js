6dfe28a257c8d2b94955a814b2a7e3e78024de06
Svetlana <Lanagoncharova30@gmail.com>
2022-10-31 14:28:46 +0300

    hw3

---
+++ JavaScript/hw3/script4.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let whidth = 1024;
+let margin = 5 * 2;
+
+let allImg = prompt("Сколько изображений в галерее?", 8);
+let whidthImg = prompt("Какая ширина каждого изображения?", 400);
