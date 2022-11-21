47ec5b5466e3e8401f93f1d44ceac1e882e9d82c
Svetlana <Lanagoncharova30@gmail.com>
2022-11-21 22:53:56 +0300

    hw9

---
+++ JavaScript/hw9/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let person = {
+  surname: "Гончарова",
+  name: "Светлана",
+  middleName: "Александровна",
+  yearOfBirth: 1991,
+  profession: "Банковская деятельность",
+  function: "Экономист",
+  experience: 7,
+};
+
+let obj = {};
+for (let key in person) {
+  obj[key] = person[key];
+}
+
+delete person.middleName;
+
+if (person[middleName] === true) {
+  alert(person.middleName);
+} else {
+  alert("не существует");
+}
+
+alert(
+  `оставшееся количество свойств у объекта person:  ${Object.values(person)}`
+);
+
+let amountNum = () => {
+  let sum = 0;
+  for (let key in person) {
+    if (typeof person[key] === "number") {
+      sum++;
+    }
+  }
+  return sum;
+};
