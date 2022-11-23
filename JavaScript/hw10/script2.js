9cd539bb9cde2541e27036691cbded055fe73d63
Svetlana <Lanagoncharova30@gmail.com>
2022-11-24 00:12:10 +0300

    hw10

--- JavaScript/hw10/script2.js
+++ JavaScript/hw10/script2.js
@@ -2,6 +2,36 @@
 
 let arr = [2, -15, 12, 83, -29, 111, -6, 22];
 
-arr.findIndex((i) => i == 111);
+let b = arr.findIndex((item) => {
+  if (item > 99) {
+    return true;
+  }
+});
 
-console.log();
+console.log(b);
+
+let с = arr.find((item) => {
+  if (item % 5 == 0) {
+    return true;
+  }
+});
+
+console.log(с);
+
+let positNum = arr.filter((item) => item >= 0);
+console.log(positNum);
+
+arr.sort((a, b) => a - b);
+console.log(arr);
+
+arr.sort((a, b) => b - a);
+console.log(arr);
+
+arr.reverse();
+console.log(arr);
+
+let x = arr.reduce((total, amount) => total * amount);
+console.log(x);
+
+let num = arr.reduce((a, b) => a + b) / arr.length;
+console.log(num);
