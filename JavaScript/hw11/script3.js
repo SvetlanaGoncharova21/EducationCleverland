fc113b47e4e8a1c318961f5cea1f0df32b1488ac
Svetlana <Lanagoncharova30@gmail.com>
2022-12-01 10:34:35 +0300

    hw11

---
+++ JavaScript/hw11/script3.js
@@ -0,0 +1,19 @@
+"uase strict";
+
+const months = [
+  "январь",
+  "февраль",
+  "март",
+  "апрель",
+  "май",
+  "июнь",
+  "июль",
+  "август",
+  "сентябрь",
+  "октябрь",
+  "ноябрь",
+  "декабрь",
+];
+const date = new Date();
+const month = date.getMonth();
+alert(months[month]);
