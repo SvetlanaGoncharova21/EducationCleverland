96061be0ff6343fc1b9a6634f5102f1e92f04cec
Svetlana <Lanagoncharova30@gmail.com>
2022-12-07 13:36:44 +0300

    hw12

---
+++ JavaScript/hw12/script3.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let list = document.querySelectorAll("li ol");
+
+list[0].style.backgroundColor = "blue";
+list[1].style.backgroundColor = "green";
+list[2].style.backgroundColor = "yellow";
+list[3].style.backgroundColor = "orange";
